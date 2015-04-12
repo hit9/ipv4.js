755c49cdbe659cde699d3b709d494b2f7a9f8b7a
hit9 <hit9@icloud.com>
2015-04-12 14:44:22 +0800

    Add network and xor

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ var util          = require('util');
 var createError   = require('create-error.js');
 
 // errors
-var AddressError      = createError('AddressError');
-var AddressTypeError  = createError('AddressTypeError', AddressError);
-var AddressValueError = createError('AddressValueError', AddressError);
+var IPv4Error      = createError('IPv4Error');
+var IPv4TypeError  = createError('IPv4TypeError', IPv4Error);
+var IPv4ValueError = createError('IPv4ValueError', IPv4Error);
+var NetValueError  = createError('NetValueError', IPv4Error);
 
 // address to integer
 function aton(addr) {
   if (typeof addr !== 'string')
-    throw new AddressTypeError('string required');
+    throw new IPv4TypeError('string required');
 
   var octs = addr.split('.');
 
   if (octs.length !== 4)
-    throw new AddressValueError('4 octects required');
+    throw new IPv4ValueError('4 octects required');
 
   var num = 0;
 
@@ -27,7 +28,7 @@ function aton(addr) {
 
     if (octi !== octn || octi !== (octi & 0xff) ||
         octi.toString() !== oct)
-      throw AddressValueError(util.format('bad octect %s', oct));
+      throw IPv4ValueError(util.format('bad octect %s', oct));
 
     num += octi << ((4 - 1 - i) * 8);
   }
@@ -37,7 +38,7 @@ function aton(addr) {
 // integer to string
 function ntoa(num) {
   if (typeof num !== 'number' || num !== parseInt(num, 10))
-    throw new AddressTypeError('integer required');
+    throw new IPv4TypeError('integer required');
 
   var octs = [];
 
@@ -58,17 +59,57 @@ function or(addra, addrb) {
   return ntoa(aton(addra) | aton(addrb));
 }
 
+function xor(addra, addrb) {
+  return ntoa(aton(addra) ^ aton(addrb));
+}
+
 // next ip address
 function next(addr, incr) {
   return ntoa(aton(addr) + (incr || 1));
 }
 
+// prev ip address
+function prev(addr, decr) {
+  return ntoa(aton(addr) - (decr || 1));
+}
+
+function network(_net) {
+  var list = _net.split('/')
+
+  if (list.length !== 2)
+    throw new NetValueError('bad network');
+
+  var net = {};
+
+  net.address = list[0];
+  net.bitmask = +list[1];
+
+  if (parseInt(net.bitmask, 10) !== net.bitmask ||
+      (net.bitmask & 31) !== net.bitmask)
+    throw new NetValueError('bad bitmask');
+
+  net.mask = ntoa(0xffffffff >> (32 - net.bitmask)
+              << (32 - net.bitmask))
+  net.hostmask = not(net.mask);
+  net.broadcast = or(net.address, net.hostmask);
+  net.first = next(xor(net.hostmask, net.broadcast));
+  net.last = prev(net.broadcast);
+  net.size = aton(net.last) - aton(net.first) + 1;
+  return net;
+}
+
+// build
+
 // exports
-exports.aton              = aton;
-exports.ntoa              = ntoa;
-exports.not               = not;
-exports.or                = or;
-exports.next              = next;
-exports.AddressError      = AddressError;
-exports.AddressTypeError  = AddressTypeError;
-exports.AddressValueError = AddressValueError;
+exports.aton           = aton;
+exports.ntoa           = ntoa;
+exports.not            = not;
+exports.or             = or;
+exports.xor            = xor;
+exports.next           = next;
+exports.prev           = prev;
+exports.network        = network;
+exports.IPv4Error      = IPv4Error;
+exports.IPv4TypeError  = IPv4TypeError;
+exports.IPv4ValueError = IPv4ValueError;
+exports.NetValueError  = NetValueError;
